73c34807e7940b617122090b29d4b45d779f9ccf
Bill Xiang <bill28.eaton@gmail.com>
2020-07-04 23:59:42 -0700

    Create layout for spigot plugin

--- spigot-waffle/src/com/bxiang/spigotwaffle/Main.java
+++ spigot-waffle/src/com/bxiang/spigotwaffle/Main.java
@@ -4,4 +4,17 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 	
+	@Override
+	public void onEnable() {
+		// server startup, reloads, plugin reloads
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		// shutdown, reloads (when plugin disabled)
+		
+	}
+	
+	// Plan: /hello => Hey welcome!
 }
