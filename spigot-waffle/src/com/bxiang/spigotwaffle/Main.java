9e4cb93f0409eb1fbed45b4766aff77541cb764a
Bill Xiang <bill28.eaton@gmail.com>
2020-07-09 14:37:14 -0700

    Finish basic greeting plugin

--- spigot-waffle/src/com/bxiang/spigotwaffle/Main.java
+++ spigot-waffle/src/com/bxiang/spigotwaffle/Main.java
@@ -1,5 +1,9 @@
 package com.bxiang.spigotwaffle;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
@@ -16,5 +20,25 @@ public class Main extends JavaPlugin {
 		
 	}
 	
-	// Plan: /hello => Hey welcome!
+	// Plan: /hello => Welcome to Bill's Server!
+		
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (label.equalsIgnoreCase("hello")) { // label equivalent to cmd.getName()
+			if (sender instanceof Player) {
+				Player player = (Player) sender;
+				if (player.hasPermission("hello.use")) {
+					player.sendMessage(ChatColor.DARK_PURPLE + "" + ChatColor.BOLD + "Welcome to Bill's Server!");
+					player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&1H&2a&3v&4e &5f&6u&7n&8!"));
+					return true;
+				}
+				player.sendMessage(ChatColor.RED + "You do not have permission.");
+				return true;
+			} else {
+				sender.sendMessage("Hello console!");
+				return true;
+			}
+		}
+		
+		return false;
+	}
 }
